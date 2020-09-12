e77d4a7f8fb24278a4dc4677de56a8ef315e7e5b
Decryptor <45062451+Abdulkbk@users.noreply.github.com>
2020-09-12 22:37:03 +0100

    heroku deploy

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+	accountSid: 'AC1ec8e90a184eb06083c04d6975b06aaa',
+	authToken: '94522116806fa37c906f6271ad4f58f5',
+	apiKey: 'SKb417ebb5e5e6624f894a0dfab02cf06e',
+	secret: 'Ik6dBviQ6wGOwZKxp521NoIQsufKihSW',
+	mongoURI: 'mongodb://127.0.0.1:27017',
+	jwtSecret: 'tm_mysecret'
+}
